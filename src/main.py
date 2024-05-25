58f9016171d3c632f5fd1fdca004c8180f130803
college-akashrai <1nt21ec008.akash@nmit.ac.in>
2024-05-25 23:58:12 +0530

    Improved the prompts

--- src/main.py
+++ src/main.py
@@ -4,15 +4,12 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 import os
 import traceback
-from pyngrok import ngrok
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from pinecone import Pinecone
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from openai import AsyncOpenAI
-import asyncio
 from typing import Dict, List, Optional
 import aiofiles
 from fastapi import FastAPI, File, Form, UploadFile, Depends, HTTPException, Request
@@ -20,14 +17,18 @@ import json
 from data_loader import load_documents
 from upload import upload_data
 import logging
-
+# from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
+# inference_api_key=os.getenv("HF_INFERENCE_API_KEY")
 
+# embeddings = HuggingFaceInferenceAPIEmbeddings(
+#     api_key=inference_api_key, model_name="BAAI/bge-large-en"
+# )
 app = FastAPI()
 
 origins = ["*"]
@@ -55,7 +56,7 @@ def embed_text(texts):
 text_field = "text"
 logging.info("Vector store successfully initialized with Pinecone and LangChain.")
 
-namespace = 'default'
+namespace = 'yashsrivastava634@gmail.com'
 
 class Query(BaseModel):
     query: str
@@ -63,9 +64,10 @@ class Query(BaseModel):
 class Namespace(BaseModel):
     set_namespace: str
 
-@app.get("/set_namespace")
-async def set_namespace(set_namespace: Namespace):
-    namespace = set_namespace.set_namespace
+@app.post("/set_namespace")
+async def set_namespace(setNamespace: Namespace):
+    global namespace
+    namespace = setNamespace.set_namespace
     logging.info(f"Namespace set to {namespace}")
     return {"message": "Namespace set successfully", "namespace": namespace}
 
@@ -104,6 +106,71 @@ async def upload_files(
         logging.error(f"Error uploading files,\n {traceback.format_exc()}")
         raise e
 
+@app.post("/get_disease")
+async def get_disease():
+    try:
+        query = "List all the diseases and problems that the patient is suffering from"
+        logging.info(f"Received query: {query}")
+        query_values = model.encode(query).tolist()
+
+        query_response = index.query(
+            top_k=10,
+            vector=query_values,
+            include_values=True,
+            include_metadata=True,
+            namespace=namespace,
+        )
+
+        context = ""
+        for match in query_response["matches"]:
+            context += match["metadata"]["text"] + "\n"
+
+        max_tokens = 3800
+        context_tokens = context.split()
+        if len(context_tokens) > max_tokens:
+            context = ' '.join(context_tokens[:max_tokens])
+
+        logging.info(f"Constructed context: {context}")
+        # prompt = f"Context:\n{context}\n\nQuestion: {query}\n\nAnswer:"
+        prompt_template = """
+        Context: {context}
+        Question: {question}
+
+        Based on the context provided, list all the diseases and problems that the patient is suffering from. 
+        Do not include diseases or problems that are explicitly stated as negative or absent. 
+        Return the diseases and problems as a comma-separated list in square brackets.
+        
+        Helpful answer:
+        """
+        prompt = prompt_template.format(context=context, question=query)
+
+        client = AsyncOpenAI(api_key=openai_api_key)
+
+        completion = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content":''' 
+                You are a highly knowledgeable and specialized medical professional with expertise in interpreting and analyzing medical reports. 
+                Your task is to provide precise, detailed, and helpful answers based on the provided medical context. Ensure your responses are 
+                strictly confined to the medical information contained in the context and avoid any general or unrelated information.
+                Focus on identifying diseases and problems that are positively indicated in the reports, and exclude any that are stated as negative or absent.
+                '''},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=4000,
+            temperature=0.7
+        )
+
+        answer = completion.choices[0].message.content
+        logging.info(f"Generated answer: {answer}")
+
+        response_data = {"answer": answer}
+        return JSONResponse(response_data)
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
+        return JSONResponse(content={"error": str(e)})
+
+
 @app.post("/get_response")
 async def get_response(user_query: Query):
     try:
@@ -129,13 +196,23 @@ async def get_response(user_query: Query):
             context = ' '.join(context_tokens[:max_tokens])
 
         logging.info(f"Constructed context: {context}")
-        prompt = f"Context:\n{context}\n\nQuestion: {query}\n\nAnswer:"
+        # prompt = f"Context:\n{context}\n\nQuestion: {query}\n\nAnswer:"
         prompt_template = """
+        You are a highly knowledgeable and specialized medical professional with expertise in interpreting and analyzing medical reports. Your task is to provide precise, detailed, and helpful answers based on the provided medical context. Ensure your responses are strictly confined to the medical information contained in the context and avoid any general or unrelated information. Your answers should be long, detailed, and easy for amateurs to understand, explaining medical terms and concepts where necessary.
+
         Context: {context}
+
         Question: {question}
 
-        Only return the helpful answer below.
-        Helpful answer:
+        Guidelines:
+        1. Carefully consider the medical context provided and ensure your answer is directly derived from this context.
+        2. If the context mentions multiple conditions but clarifies some as negative, exclude those from your considerations.
+        3. Provide detailed medical advice or information relevant to the question, ensuring it aligns with the medical data in the context.
+        4. Explain medical terms and concepts in a way that is easy for amateurs to understand.
+        5. Provide long and detailed answers that are comprehensive and educational.
+        6. Do not provide answers outside the medical domain or unrelated to the context.
+
+        Answer:
         """
         prompt = prompt_template.format(context=context, question=query)
 
@@ -144,7 +221,12 @@ async def get_response(user_query: Query):
         completion = await client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "system", "content": '''
+                 You are a highly knowledgeable and specialized medical professional with expertise in interpreting and analyzing medical reports. 
+                Your task is to provide precise, detailed, and helpful answers based on the provided medical context. Ensure your responses are 
+                strictly confined to the medical information contained in the context and avoid any general or unrelated information. Your answers 
+                should be long, detailed, and easy for amateurs to understand, explaining medical terms and concepts where necessary.
+                 '''},
                 {"role": "user", "content": prompt}
             ],
             max_tokens=4000,
